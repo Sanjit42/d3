b79ba7069eb1cb408f964ac933b8d3c6ad9e2a7b
Sanjit <sanjitprad@gmail.com>
2016-11-18 19:06:26 +0530

    Create hollow half pie chart

---
+++ hollow-pie-chart/hollowPieChart.js
@@ -0,0 +1,36 @@
+const WIDTH = 800;
+const HEIGHT = 500;
+const RADIUS = Math.min(WIDTH, HEIGHT)/2;
+
+var data = [1,1,2,2,1,2,1];
+
+var color = d3.scaleOrdinal().range(["#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C", "#98DF8A", "#D62728"]);
+
+var loadChart = function () {
+    var svg = d3.select('.container').append("svg")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT);
+
+        var g = svg.append("g")
+        .attr("transform", "translate(" + WIDTH / 2 + "," + HEIGHT / 2 + ")");
+
+    var arc = d3.arc()
+        .outerRadius(RADIUS - 10)
+        .innerRadius(120);
+    
+    var pie = d3.pie()
+        .sort(null)
+        .value(function (d) { return d});
+
+    g.selectAll('path')
+        .data(pie(data))
+        .enter()
+        .append('path')
+        .attr("d", arc)
+        .style('fill', function (d, i) {
+            return color(i)
+        })
+};
+
+
+window.onload = loadChart;
